3fef45efd7d0cd73c073fdd9e93fc348ed760c5e
famisanar_jgomezr <jgomezr@famisanar.com.co>
2026-06-15 21:54:50 -0500

    Cambios Video

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -6,7 +6,7 @@ from pathlib import Path
 class Settings:
     app_name: str = "Core Banking Lite"
     app_env: str = os.getenv("APP_ENV", "development")
-    app_version: str = os.getenv("APP_VERSION", "2.0.0")
+    app_version: str = os.getenv("APP_VERSION", "3.0.0")
     data_dir: Path = Path(os.getenv("DATA_DIR", "./data"))
 
 
